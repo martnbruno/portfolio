fbedfe9a40656e78261967229bb3c6c7fea1457c
m-bruno <martinbruno.mb@outlook.com>
2021-05-08 19:43:05 -0300

    adding burger menu

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ backToTop.addEventListener ("click", () => {
     window.scrollTo(0,0)
     header.focus()
 })
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle Now
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = ''
+        } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
+        } 
+     });
+
+    // Burger Animation
+     burger.classList.toggle('toggle')
+
+    });
+    
+}
+
+navSlide();
